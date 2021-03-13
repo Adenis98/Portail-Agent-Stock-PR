f8e7589f3105ed536777cc2574e39df29ef75b27
Ramez <ramezzormati1998@gmail.com>
2021-03-13 03:19:12 +0100

    animation appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'], 
+  animations:[
+    trigger('circleShapeFade',[
+      transition('* => void',animate('0.7s 0.7s ease-in',
+        style({transform :'translateX(400%)' })
+        )
+      )
+    ]),
+    trigger('botShapeFade',[
+      transition('* => void',animate('0.7s 0.3s ease-in',
+        style({transform :'translateY(200%)' })
+        )
+      )
+    ])
+  ]
 })
 export class AppComponent {
   title = 'hello';
