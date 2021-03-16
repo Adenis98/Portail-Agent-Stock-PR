40ee54d82a9bfeab8bfe0069bd3dc08c58c80ad1
Ramez <ramezzormati1998@gmail.com>
2021-03-16 23:05:45 +0100

    table compte

--- src/app/services/comptes/comptes.service.ts
+++ src/app/services/comptes/comptes.service.ts
@@ -5,11 +5,14 @@ import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
 })
 export class GetcomptesService {
 
+  public headers_object ;
   constructor(private http: HttpClient) {
+    if(localStorage.getItem("jwt")!=null)
+      this.headers_object = new HttpHeaders().append("Authorization","Bearer "+localStorage.getItem("jwt"));
    }
 
   getComptes() {
-    let url = "http://localhost:8080/compte/avoirTout"
-    return this.http.get(url, { headers:new HttpHeaders().append("Authorization","Bearer "+localStorage.getItem("jwt")) })
+    let url = "http://localhost:8080/compte/avoirTout";
+    return this.http.get(url, { headers:this.headers_object });
   }
 }
