3577891b342390dabae1aabdcde9c023a6397db5
youssef <jozefjobrane@gmail.com>
2021-03-27 04:15:23 +0100

    consultation Pr

--- src/app/components/sidebarcpnts/page3/page3.component.ts
+++ src/app/components/sidebarcpnts/page3/page3.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page3.component.css']
 })
 export class Page3Component implements OnInit {
-
+  qte:string="";
   constructor() { }
 
   ngOnInit(): void {
+    this.qte="1";
+  }
+  addQte()
+  {
+    this.qte=(parseInt(this.qte)+1).toString();
+  }
+  removeQte()
+  {
+    if(parseInt(this.qte)>1)
+    this.qte=(parseInt(this.qte)-1).toString();
   }
 
 }
