415d3724f7575672d3d87ddb61190fc8d63f910b
youssef <jozefjobrane@gmail.com>
2021-04-08 02:31:00 +0100

    commande ligne update

--- src/app/components/sidebarcpnts/commande/page2.component.ts
+++ src/app/components/sidebarcpnts/commande/page2.component.ts
@@ -6,17 +6,28 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './page2.component.html',
   styleUrls: ['./page2.component.css'],
   animations: [
-    trigger('lignePanierAnim', [
+    trigger('ligneCommandAnim', [
       transition('* => void', animate('0.7s 0.2s ease-in',
         style([{ transform: 'translateX(200%)' }])
       )
       ),
       transition('void => *',
-        [style([{ transform: 'translateX(-200%)' }])
+        [style([{ transform: 'translateX(-200%)' ,opacity:'0'}])
           , animate('0.5s 0.2s ease-out'
           )]
       )
     ]),
+    trigger('morDetailAnim', [
+      transition('* => void', animate('0.7s 0.3s ease-in',
+        style([{ transform: 'translateY(-110%)' }, {opacity:0 }])
+      )
+      ),
+      transition('void => *',
+        [style([{ transform: 'translateY(-110%)' ,opacity:0}])
+          , animate('0.5s 0.3s ease-out'
+          )]
+      )
+    ])
   ]
 })
 export class Page2Component implements OnInit {
